3ba3e6528d5727f894331e506700b3d394169d50
Faruk Suljagic <faruksuljagic@Faruks-MacBook-Pro.local>
2022-11-13 17:49:04 +0100

    Push v2

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-import {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, View, Button, TextInput, FlatList } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('')
-  const [courseGoals, setCourseGoals] = useState([])
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  };
+  const [courseGoals, setCourseGoals] = useState([]);
+  
 
-  function addGoalHandler() {
-    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
-  };
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
+
+  function deleteGoalHandler() {
+    console.log('delete');
+  }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder='Your course goal!' style={styles.textInput} onChangeText={goalInputHandler}/>
-              <Button title='Add Goal!' onPress={addGoalHandler}/>
-       </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <View key={goal + Math.random()} style={styles.goalItem}><Text style={styles.goalText}>{goal}</Text></View> )}
-      </View> 
+        <FlatList
+          data={courseGoals}
+          renderItem={(item) => {
+            return <GoalItem text={item.item.text} onDeleteItem={deleteGoalHandler} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+          // keyboardShouldPersistTaps='handled'
+          keyboardDismissMode="onDrag"
+        ></FlatList>
+      </View>
     </View>
   );
 }
@@ -30,21 +44,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
-    backgroundColor: '#f1f1f1'
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
+    backgroundColor: "#f1f1f1",
   },
   goalsContainer: {
     flex: 4,
@@ -52,14 +52,4 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     paddingTop: 10,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
-    fontSize: 16,
-  }
 });
